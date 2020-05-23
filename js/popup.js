18090728e567e49e07dec9ad5f17690b671267ff
takeahot <mirtrudmaynow@gmail.com>
2020-05-23 16:29:55 +0300

    use fetch and create cPF.userGetList in sketch

--- js/popup.js
+++ js/popup.js
@@ -1,59 +1,28 @@
 class cPF {
     constructor(apiKey,token,account,url) {
-        this.parse = new DOMParser();
-        this.xr_data = {};
-        this.xr_data.url = url;
-        this.xr_data.account = account;
-        this.xr_data.headers = {
-            Authorization: "Basic " + btoa(apiKey + ":" + token),
-            Accept: "application/xml",
-            "Content-Type": "application/xml"
-        }
+        this.connParam = [apiKey,token,account,url];
     }
-    SendReq () {
-        let xr = new XMLHttpRequest();
-        xr.onload = function () {
-        return this.responseXML;
+    generFetch (body) {
+    return fetch(this.connParam[3],{
+        method:'POST',
+        headers: {
+            'Authorization': "Basic " + btoa(this.connParam[0] + ":" + this.connParam[1]),
+            'Accept': 'application/xml',
+            'Content-Type': 'application/xml'
+        },
+        body: body})
+    .then(response => response.text())
+    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    .then(data => console.log(data))
     }
-
-        };
-        this.xr.open('POST','https://apiru.planfix.ru/xml/',true);
-    getUserList () {
-        reqBody = '<request method="user.getList">'
-            + '<account>acgm</account>'
-            + '<status>ACTIVE</status>'
-            +  "</request>";
-        console.log(reqBody);
-        xr = parse.parseFromString(reqBody,'text/xml');
-        this.xr.send(xr);
-    }
-
-    xr () {
-
+    userGetList () {
+        return this.generFetch('<request method="user.getList">' + '<account>' + this.connParam[2] + '</account>' + '</request>')
     }
 }
 
-
 $(function () {
-    if ( typeof parse == 'undefined' ) {
-        var parse = new DOMParser();
+        let ares = new cPF('e98af7c15042cc61ca26e0537383eb03','0f816577be7d7c7c79aec532693cc84c','acgm','https://apiru.planfix.ru/xml/');
+        ares.userGetList();
     }
-    var a = new XMLHttpRequest();
-    var xr = 'xmlDoc';
-    a.onload = function () {
-        document.getElementById('user_list').innerHTML = this.responseXML;
-        console.log(this.responseXML)
-    };
-    a.open('POST','https://apiru.planfix.ru/xml/',true);
-    a.setRequestHeader("Authorization", "Basic " + btoa("e98af7c15042cc61ca26e0537383eb03" + ":" + "0f816577be7d7c7c79aec532693cc84c"));
-    a.setRequestHeader('Accept','application/xml');
-    a.setRequestHeader('Content-Type','application/xml');
-    reqBody = '<request method="user.getList">'
-                  + '<account>acgm</account>'
-                  + '<status>ACTIVE</status>'
-                  +  "</request>";
-    console.log(reqBody);
-    xr = parse.parseFromString(reqBody,'text/xml');
-    a.send(xr);
-})
+)
 
